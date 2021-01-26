5ebe356a90057666d078efad8a9d1e126d23157e
Ramon Moreira <ramonrodriguesmo@gmail.com>
2021-01-26 09:44:37 -0300

    Criando componentes e construindo o projeto

---
+++ course-manager/src/app/courses/course-list.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from "@angular/core";
+import { Course } from "./course";
+
+@Component({
+    selector: 'app-course-list',
+    templateUrl: './course-list.component.html'
+})
+export class CourseListComponent implements OnInit{
+    courses: Course[] = [];
+
+    ngOnInit(): void {
+        this.courses = [
+            {
+                id: 1,
+                name: 'Angular: Forms',
+                imageUrl: '',
+                price: 99.99,
+                code: 'XPS-8796',
+                duration: 120,
+                rating: 4.5,
+                releaseDate: 'November, 2, 2019'
+            },
+            {
+                id: 2,
+                name: 'Angular: HTTP',
+                imageUrl: '',
+                price: 45.99,
+                code: 'LKL-1094',
+                duration: 80,
+                rating: 4,
+                releaseDate: 'December, 4, 2019'
+            }
+        ]
+    }
+}
