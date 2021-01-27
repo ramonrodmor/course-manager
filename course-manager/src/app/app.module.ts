f6c6a0c5ffd13e0bed9552b88d0f28cd8980edda
Ramon Moreira <ramonrodriguesmo@gmail.com>
2021-01-27 12:22:52 -0300

    Utilização da pasta core para modularizar componentes mais robustos

--- course-manager/src/app/app.module.ts
+++ course-manager/src/app/app.module.ts
@@ -5,14 +5,13 @@ import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http'
 
 import { AppComponent } from './app.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { Error404Component } from './error-404/error-404.component';
 import { CourseModule } from './courses/course.module';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent,
     Error404Component
   ],
   imports: [
@@ -20,6 +19,7 @@ import { CourseModule } from './courses/course.module';
     FormsModule,
     HttpClientModule,
     CourseModule,
+    CoreModule,
     RouterModule.forRoot([
       {
         path: '', redirectTo: 'courses', pathMatch: 'full'
